02d416d0836d81837bed43e5d9e6ceea4bed9ffb
котяра <gamerkot306@gmail.com>
2024-10-12 15:16:36 +0300

    -=

--- youactive/target/debug/build/youactive-e38cc81e3300ce4e/build_script_build-e38cc81e3300ce4e.d
+++ youactive/target/debug/build/youactive-e38cc81e3300ce4e/build_script_build-e38cc81e3300ce4e.d
@@ -1,5 +1,5 @@
-c:\Users\JONNY\Documents\GitHub\YouActive\youactive\target\debug\build\youactive-e38cc81e3300ce4e\build_script_build-e38cc81e3300ce4e.exe: build.rs
+C:\Users\JONNY\Documents\GitHub\YouActive\youactive\target\debug\build\youactive-e38cc81e3300ce4e\build_script_build-e38cc81e3300ce4e.exe: build.rs
 
-c:\Users\JONNY\Documents\GitHub\YouActive\youactive\target\debug\build\youactive-e38cc81e3300ce4e\build_script_build-e38cc81e3300ce4e.d: build.rs
+C:\Users\JONNY\Documents\GitHub\YouActive\youactive\target\debug\build\youactive-e38cc81e3300ce4e\build_script_build-e38cc81e3300ce4e.d: build.rs
 
 build.rs:
